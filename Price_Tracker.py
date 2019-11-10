85d1e0bf8cffe8dbf7c85325a602ef5dc1192d13
Stanley Pan <imoment.developer@gmail.com>
2019-11-11 06:23:10 +0900

    Complete

--- Price_Tracker.py
+++ Price_Tracker.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import time
 
 URL = 'https://www.amazon.com/Apple-MacBook-15-inch-512GB-Storage/dp/B07S58MHXF/ref=sr_1_1?keywords=New+Apple+MacBook+' \
       'Pro+%2815-inch%2C+16GB+RAM%2C+512GB+Storage%29+-+Space+Gray&qid=1573416231&sr=8-1'
@@ -17,11 +19,49 @@ def check_price():
     adjusted_price = product_price.replace(",", "")
     converted_price = float(adjusted_price[1:10])
 
-    if (converted_price < 2499.99):
+    if (converted_price > 2499.98):
         send_email()
 
         print("Product title is: " + product_title)
         print("Product price is: " + str(converted_price))
 
 def send_email():
-    print("Email has been sent.")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('imoment.developer@gmail.com', 'hkcgkvwwyzsrhddc')
+
+    subject = 'STANLEY, THE  MACBOOK PRO PRICE FALL DOWN SOMETHING'
+    body = 'Check this link fucker: https://www.amazon.com/Apple-MacBook-15-inch-512GB-Storage/dp/' \
+           'B07S58MHXF/ref=sr_1_1?keywords=New+Apple+MacBook+Pro+%2815-inch%2C+16GB+RAM%2C+512GB+Sto' \
+           'rage%29+-+Space+Gray&qid=1573416231&sr=8-1'
+
+    message = "Subject: " + subject + "\n\n" + body
+
+    server.sendmail(
+        'imoment.developer@gmail.com',
+        'imoment.developer@gmail.com',
+        message
+    )
+    print('Email has been sent.')
+
+    server.quit()
+
+check_price()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
